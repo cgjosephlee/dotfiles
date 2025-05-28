cf48b1f0bd60d14f0bd8fd7921fe14462168880b
Joseph Lee <cgjosephlee@gmail.com>
2025-05-28 10:35:05 +0800

    update nvim, fix spelling issue in vscode

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = "lazyvim_wrap_spell",
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    if vim.g.vscode then
+      vim.opt_local.wrap = false
+      vim.opt_local.spell = false
+    else
+      vim.opt_local.wrap = true
+      vim.opt_local.spell = true
+    end
+  end,
+})
